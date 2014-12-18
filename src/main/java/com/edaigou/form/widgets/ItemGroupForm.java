043e8553845e41673e74f31c808521cd340215e7
huangpengpeng <330659459@qq.con>
2014-12-18 09:43:11 +0800

    init

--- src/main/java/com/edaigou/form/widgets/ItemGroupForm.java
+++ src/main/java/com/edaigou/form/widgets/ItemGroupForm.java
@@ -86,7 +86,6 @@ public class ItemGroupForm {
 			comboOfShop.setText("");
 		image.setImage(SWTResourceManager.getImage(MainForm.class,
 				"/com/edaigou/resource/default.jpg"));
-		getComboOfPType().setText("");
 	}
 
 	public void write(Map map) throws MalformedURLException {
